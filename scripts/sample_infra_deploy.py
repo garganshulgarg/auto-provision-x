f60acd19ea9144ed31019523280d7a801d1710a0
garganshulgarg <anshulgarg.cse@gmail.com>
2023-07-03 17:11:20 +0530

    chore: updated the infra-deploy as per user selection.

--- scripts/sample_infra_deploy.py
+++ scripts/sample_infra_deploy.py
@@ -1,6 +1,8 @@
+import argparse
 import subprocess
 
 def run_terraform_init(terraform_dir):
+    """Run `terraform init` command in the specified directory."""
     try:
         subprocess.run(["terraform", "init"], check=True, cwd=terraform_dir)
     except subprocess.CalledProcessError as e:
@@ -9,6 +11,7 @@ def run_terraform_init(terraform_dir):
         exit(1)
 
 def run_terraform_plan(terraform_dir):
+    """Run `terraform plan` command in the specified directory."""
     try:
         subprocess.run(["terraform", "plan"], check=True, cwd=terraform_dir)
     except subprocess.CalledProcessError as e:
@@ -16,7 +19,40 @@ def run_terraform_plan(terraform_dir):
         print(e)
         exit(1)
 
-# Example usage
+def run_terraform_apply(terraform_dir):
+    """Run `terraform apply` command in the specified directory."""
+    try:
+        subprocess.run(["terraform", "apply", "--auto-approve"], check=True, cwd=terraform_dir)
+    except subprocess.CalledProcessError as e:
+        print("Terraform apply failed.")
+        print(e)
+        exit(1)
+
+def run_terraform_destroy(terraform_dir):
+    """Run `terraform destroy` command in the specified directory."""
+    try:
+        subprocess.run(["terraform", "destroy", "--auto-approve"], check=True, cwd=terraform_dir)
+    except subprocess.CalledProcessError as e:
+        print("Terraform destroy failed.")
+        print(e)
+        exit(1)
+
+# Create the argument parser
+parser = argparse.ArgumentParser(description="Terraform operations script")
+parser.add_argument("operation", choices=["apply", "destroy"], help="Operation to perform: apply or destroy")
+args = parser.parse_args()
+
+# Set the directory path for the Terraform project
 terraform_dir = "./samples"
-run_terraform_init(terraform_dir)
-run_terraform_plan(terraform_dir)
+
+# Perform Terraform operations
+if args.operation == "apply":
+    # Apply changes
+    run_terraform_init(terraform_dir)
+    run_terraform_plan(terraform_dir)
+    run_terraform_apply(terraform_dir)
+elif args.operation == "destroy":
+    # Destroy resources
+    run_terraform_init(terraform_dir)
+    run_terraform_plan(terraform_dir)
+    run_terraform_destroy(terraform_dir)
