ed54f8a41febf01ff70c4c7c4490d7bef29c1812
garganshulgarg <anshulgarg.cse@gmail.com>
2023-07-04 12:31:41 +0530

    chore: added multi cloud support

--- samples/create-vpc.tf
+++ samples/create-vpc.tf
@@ -1,4 +1,4 @@
-# module "aws_create_vpc" {
-#    source = "git::https://github.com/garganshulgarg/auto-provision-x.git//modules/aws/create-vpc/?ref=main"
-#    cidr_block = "10.0.0.0/16"
-# }
+module "aws_create_vpc" {
+   source = "git::https://github.com/garganshulgarg/auto-provision-x.git//modules/aws/create-vpc/?ref=main"
+   cidr_block = "10.0.0.0/16"
+}
