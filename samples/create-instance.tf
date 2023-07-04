3e227a1a99d739cb95b9907261f3dcfc080a45c9
garganshulgarg <anshulgarg.cse@gmail.com>
2023-07-04 15:25:04 +0530

    chore: update the branch name

--- samples/create-instance.tf
+++ samples/create-instance.tf
@@ -1,3 +1,3 @@
 module "gcp_create_vm" {
-  source = "git::https://github.com/garganshulgarg/auto-provision-x.git//modules/gcp/create-vm/?ref=gcp-setup"
+  source = "git::https://github.com/garganshulgarg/auto-provision-x.git//modules/gcp/create-vm/?ref=main"
 }
